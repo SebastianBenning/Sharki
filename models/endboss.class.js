f7e1bf17ddf860561a8f8af91f88d438d1705777
SebastianBenning <benning655@googlemail.com>
2023-01-13 17:14:25 +0100

    images

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -4,18 +4,7 @@ class Endboss extends MovableObject {
     width = 400;
     y = 0;
     hadfirstContact = false;
-    images_walking = [
-        'img/2.Enemy/3 Final Enemy/1.Introduce/1.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/2.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/3.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/4.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/5.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/6.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/7.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/8.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/9.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/10.png',
-    ];
+    
 
     constructor() {
         super().loadImage(this.images_walking[0]);
